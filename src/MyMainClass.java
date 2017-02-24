1b788fd6c7909ce8216b0381d0ef5549c0faa5e3
haoguang <haoguang18@hotmail.com>
2017-02-24 11:33:59 +0800

    added some new line of code to display the year of tutorial and print it out

--- src/MyMainClass.java
+++ src/MyMainClass.java
@@ -16,6 +16,7 @@ public class MyMainClass {
     public static void main(String[] args) {
         System.out.println("Hi, there");
         System.out.println("This is a git hub tutorial");
+        
     }
     
 }
